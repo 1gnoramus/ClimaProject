614ea95ade2922f6e792bc1ff92a389b60650706
1gnoramus <elenwarface@gmail.com>
2022-07-16 13:53:22 +0600

    'URL'

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -14,10 +16,34 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.latitude);
   }
 
+  void getData() async {
+    var url = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=defaa80eb6e43e947faa745459177730');
+
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var decodedData = jsonDecode(data);
+
+      double temperature = decodedData['main']['temp'];
+      String cityName = decodedData['name'];
+      int condition = decodedData['weather'][0]['id'];
+
+      print(temperature);
+      print(cityName);
+      print(condition);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
 
   @override
